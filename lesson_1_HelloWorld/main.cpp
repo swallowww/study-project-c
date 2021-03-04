0d8c3bfb103b3f5338a15a5dc41bc5c88e98573c
swallowww <1105047416@qq.com>
2021-03-04 23:53:42 +0800

    main.cpp

--- lesson_1_HelloWorld/main.cpp
+++ lesson_1_HelloWorld/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << "Hello,world!"<< endl;
+	cout << "Hello world!" << endl;
 	return 0;
 }
